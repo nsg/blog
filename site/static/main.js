91f5d0c499dc87a6b76af7543dbe53adc31e6292
Stefan Berggren <nsg@nsg.cc>
2023-08-26 00:37:17 +0200

    Redesign search

--- site/static/main.js
+++ site/static/main.js
@@ -1,7 +1,6 @@
 function init_search() {
-    let search_div = document.getElementsByClassName("search")[0];
+    let search_div = document.querySelector("#search");
     let form = search_div.getElementsByTagName("form")[0];
-    let result_div = search_div.getElementsByClassName("search-result")[0];
     let index = elasticlunr.Index.load(window.searchIndex);
     let options = {
         bool: "AND",
@@ -10,35 +9,61 @@ function init_search() {
           body: {boost: 1},
         }
       };
-    var search_div_display_state;
+
+    document.querySelector(".search button").addEventListener("click", (event) => {
+      search_div.style.display = "block";
+      event.target.style.display = "none";
+    });
 
     form.addEventListener("submit", (event) => {
-        let text = event.target.getElementsByTagName("input")[0].value;
-        let results = index.search(text, options);
-        var ul = document.createElement("ul");
-        
-        result_div.innerHTML = "";
-
-        if (results.length == 0) {
-          var li = document.createElement("li");
-          li.innerHTML = "No results found";
-          ul.appendChild(li);
-        }
-        
-        results.forEach(r => {
-          var li = document.createElement("li");
-          var a = document.createElement("a");
-          a.innerHTML = r.doc.title;
-          a.href = r.doc.id;
-          li.appendChild(a);
-          ul.appendChild(li);
-        });
-        
-        result_div.appendChild(ul);
-        result_div.style.display = "block";
-
-        event.preventDefault();
+      let text = event.target.getElementsByTagName("input")[0].value;
+      do_search(index, options, text);
+      event.preventDefault();
     });
+
+    form.addEventListener("keyup", (event) => {
+      do_search(index, options, event.target.value);
+    });
+}
+
+function do_search(index, options, text) {
+  let result_div = document.querySelector("#search .results");
+  result_div.style.opacity = "1";
+
+  let results = index.search(text, options);
+  var ul = document.createElement("ul");
+    
+  result_div.innerHTML = "";
+
+  if (results.length == 0) {
+    var li = document.createElement("li");
+    li.innerHTML = "No results found";
+    ul.appendChild(li);
+    result_div.style.height = "2em";
+  }
+
+  results.forEach(r => {
+    var li = document.createElement("li");
+    var a = document.createElement("a");
+    
+    if (r.doc.title == "") {
+      a.innerHTML = "➢ No title";
+    } else {
+      a.innerHTML = "➢ " + r.doc.title;
+    }
+
+    a.href = r.doc.id;
+    li.appendChild(a);
+    ul.appendChild(li);
+
+  });
+  
+  if (results.length > 0) {
+    result_div.style.height = Math.ceil(results.length * 2.1) + "em";
+  }
+
+  result_div.appendChild(ul);
+  result_div.style.display = "block";
 }
 
 document.addEventListener("DOMContentLoaded", init_search());
